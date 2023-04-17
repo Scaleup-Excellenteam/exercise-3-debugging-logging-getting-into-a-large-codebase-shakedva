03645cbb7fb0157f97bf1b06486f00f312113958
shakedva <shakediii123@gmail.com>
2023-04-17 21:32:43 +0300

    Add GameStatus enum

--- enums.py
+++ enums.py
@@ -4,3 +4,10 @@ class Player:
     EMPTY = -9
     PIECES = ['white_r', 'white_n', 'white_b', 'white_q', 'white_k', 'white_p',
               'black_r', 'black_n', 'black_b', 'black_q', 'black_k', 'black_p']
+
+
+class GameStatus:
+    BLACK_WON = 0
+    WHITE_WON = 1
+    STALEMATE = 2
+    IN_PROGRESS = 3
