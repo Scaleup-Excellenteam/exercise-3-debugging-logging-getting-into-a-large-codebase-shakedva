69321523d38001b3c1f1e3f42e9c36e49966a27c
shakedva <shakediii123@gmail.com>
2023-04-25 17:35:52 +0300

    Unit tests finished

--- test_chess.py
+++ test_chess.py
@@ -4,9 +4,10 @@ from Piece import Knight
 from enums import Player
 
 """
-Tests for the function get_valid_peaceful_moves in class Knight
+Unit tests
 """
 
+# Tests for the function get_valid_peaceful_moves in class Knight
 
 def test_knight_middle_empty():
     game_state_mock = Mock()
@@ -27,32 +28,38 @@ def test_knight_right_empty():
 
 def test_knight_upper_empty():
     game_state_mock = Mock()
-    game_state_mock.get_piece = Mock(return_value=Player.EMPTY)
+    # game_state_mock.get_piece = Mock(return_value=Player.EMPTY)
+    game_state_mock.get_piece.side_effect = lambda row, col: Knight('n', row, col, Player.PLAYER_1) \
+        if (row, col) in [(6, 3)] else Player.EMPTY
+
     black_knight = Knight('n', 7, 5, Player.PLAYER_2)
     result = black_knight.get_valid_peaceful_moves(game_state_mock)
     result = [i for i in result if 0 <= i[0] < 8 and 0 <= i[1] < 8]
-    assert set(result) == {(5, 4), (5, 6), (6, 3), (6, 7)}
+    assert set(result) == {(5, 4), (5, 6), (6, 7)}
 
 
-"""
-Tests for the function get_valid_piece_takes in class Knight
-"""
-
+# Tests for the function get_valid_piece_takes in class Knight
 
 def test_knight_only_take_moves():
     game_state_mock = Mock()
+    game_state_mock.is_valid_piece.side_effect = lambda row, col: True if 0 <= row < 8 and 0 <= col < 8 else False
+    game_state_mock.get_piece.side_effect = lambda row, col: Knight('n', row, col, Player.PLAYER_2) \
+        if (row, col) in [(1, 2), (1, 4), (2, 1), (2, 5), (4, 1), (4, 5), (5, 2), (5, 4)] else Player.EMPTY
 
-    def get_piece_side_effect(row, col):
-        if row == 3 and col == 3:
-            return Player.PLAYER_1
-        else:
-            return Player.PLAYER_2
+    white_knight = Knight('n', 3, 3, Player.PLAYER_1)
+    result = white_knight.get_valid_piece_takes(game_state_mock)
 
-    # game_state_mock.get_piece = Mock(return_value=get_piece_side_effect)
-    # game_state_mock.is_valid_piece = Mock(return_value=True)
-    # game_state_mock.get_player = Mock(return_value=Player.PLAYER_2)
-    # white_knight = Knight('n', 3, 3, Player.PLAYER_1)
-    # result = white_knight.get_valid_piece_takes(game_state_mock)
-    # result = [i for i in result if 0 <= i[0] < 8 and 0 <= i[1] < 8]
-    # assert set(result) == {}
+    # assert that all the available squares have a black knight on them
+    assert all(game_state_mock.get_piece(row, col).get_player() == Player.PLAYER_2 for row, col in result)
 
+
+def test_knight_upper_take_moves():
+    game_state_mock = Mock()
+    game_state_mock.is_valid_piece.side_effect = \
+        lambda row, col: True if (0 <= row < 8 and 0 <= col < 8 and (row, col) != (6, 5)) else False
+    game_state_mock.get_piece.side_effect = lambda row, col: Knight('n', row, col, Player.PLAYER_2) \
+        if (row, col) in [(5, 2), (6, 1), (5, 4)] else Player.EMPTY
+
+    white_knight = Knight('n', 7, 3, Player.PLAYER_1)
+    result = white_knight.get_valid_piece_takes(game_state_mock)
+    assert all(game_state_mock.get_piece(row, col).get_player() == Player.PLAYER_2 for row, col in result)
